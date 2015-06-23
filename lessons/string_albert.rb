067fcfb33cccccbc461ec92e65e885c3f5d89e75
Olbert <iampirate@rambler.ru>
2015-06-23 10:56:18 +0300

    New files

---
+++ lessons/string_albert.rb
@@ -0,0 +1,8 @@
+
+def length (str)
+	str.length
+	puts"your string is #{str} and it's length is #{str.length}"
+end
+
+str="qwertyuiop"
+length(str)
---
+++ lessons/string_albert2.rb
@@ -0,0 +1,3 @@
+parent_str = "your string"
+child_str = "string"
+puts parent_str.end_with?(child_str)
